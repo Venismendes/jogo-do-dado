d9b1e5303faf7af4dca6a74a1d5ac080f16682e9
Venismendes <venismendes@gmail.com>
2020-10-09 00:33:35 -0300

    nomes de variaveis alterados

--- jogo do dado.py
+++ jogo do dado.py
@@ -1,51 +1,51 @@
 import random
 
-pontos_jogador1 = 1000
-pontos_jogador2 = 1000
+player1_points = 1000
+player2_points = 1000
 dado = (1, 2, 3, 4, 5, 6)
-jogadores = 1
+players = 1
 
-jogador = input('Olá qual o seu nome: ')
-print(f'Bem vindo ao jogo do dado {jogador}')
+player = input('Olá qual o seu nome: ')
+print(f'Bem vindo ao jogo do dado {player}')
 
 
 quantidade_jogadores = input('Deseja jogador de 2 jogadores? [S/N]: ').upper()
 
 
 if quantidade_jogadores == 'S':
-    jogadores = 2
-    jogador1 = jogador
-    jogador2 = input('Nome do segundo jogador: ')
+    players = 2
+    player1 = player
+    player2 = input('Nome do segundo jogador: ')
 
 
 print('=' * 40)
-while jogadores == 1:
+while players == 1:
     print('Inicialmente você tem 1000 pontos!')
     print('[S] para sair!')
     print('[P] para ver seus pontos')
     print('[J] para jogar: ')
     while True:
         print('=' * 30)
-        opcao = input('Opção: ').upper()
+        option = input('Opção: ').upper()
         print('=' * 30)
 
         while True:
             menu = True
-            na = random.choice(dado)
-            if na <= 3:
-                valor = 'L'
+            dice = random.choice(dado)
+            if dice <= 3:
+                value = 'L'
             else:
-                valor = 'H'
+                value = 'H'
 
-            if opcao == 'J':
+            if option == 'J':
                 while True:
                     try:
-                        if pontos_jogador1 <= 0:
+                        if player1_points <= 0:
                             print('Fim do jogo!!')
                             exit()
-                        aposta = int(input('Quantos pontos: '))
-                        if aposta <= 0 or aposta > pontos_jogador1:
-                            print(f'O valor deve ser maior que 0, seus pontos são {pontos_jogador1}: ')
+                        bet = int(input('Quantos pontos: '))
+                        if bet <= 0 or bet > player1_points:
+                            print(f'O valor deve ser maior que 0, seus pontos são {player1_points}: ')
 
                         else:
                             low_high = input('|[L] = 123 | [H] = 456 | [S] = Sair: ').upper()
@@ -59,13 +59,13 @@ while jogadores == 1:
                     low_high = input('|[L] = 123 | [H] = 456 | [S] = Sair: ').upper()
 
                 
-                if low_high == valor and (low_high == 'L' or low_high == 'H'):
-                    print(f'Você acertou e ganhou {aposta} pontos!!')
-                    pontos_jogador1 = pontos_jogador1 + aposta
+                if low_high == value and (low_high == 'L' or low_high == 'H'):
+                    print(f'Você acertou e ganhou {bet} pontos!!')
+                    player1_points = player1_points + bet
                     menu = False
                 elif low_high == 'L' or low_high == 'H':
-                    print(f'Você errou e perdeu {aposta} pontos!!')
-                    pontos_jogador1 = pontos_jogador1 - aposta
+                    print(f'Você errou e perdeu {bet} pontos!!')
+                    player1_points = player1_points - bet
                     menu = False
                     
                 if low_high == 'S':
@@ -73,61 +73,61 @@ while jogadores == 1:
                     break
                  
                 print()
-                print(f'Dado: {na} = {valor}')
+                print(f'Dado: {dice} = {value}')
 
-                print(f'Seus pontos: {pontos_jogador1}')
+                print(f'Seus pontos: {player1_points}')
                 print('=' * 30)
 
-            if opcao == 'S':
+            if option == 'S':
                 exit()
-            if opcao == 'P':
-                print(f'Você possui {pontos_jogador1} pontos.')
+            if option == 'P':
+                print(f'Você possui {player1_points} pontos.')
 
             if menu == True:
                 print('=' * 30)
-                opcao = input('Opção: ').upper()
+                option = input('Opção: ').upper()
                 print('=' * 30)
 print('=' * 40)
-while jogadores == 2:
+while players == 2:
     print('Inicialmente ambos tem 1000 pontos!')
     print('[S] para sair!')
-    print(f'[P1] para ver seus pontos {jogador1}')
-    print(f'[P2] para ver seus pontos {jogador2}')
+    print(f'[P1] para ver seus pontos {player1}')
+    print(f'[P2] para ver seus pontos {player2}')
     print('[J] para jogar: ')
     while True:
         print('=' * 30)
-        opcao = input('Opção: ').upper()
+        option = input('Opção: ').upper()
         print('=' * 30)
 
         while True:
             menu = True
-            na = random.choice(dado)
-            if na <= 3:
-                valor = 'L'
+            dice = random.choice(dado)
+            if dice <= 3:
+                value = 'L'
             else:
-                valor = 'H'
+                value = 'H'
 
-            if opcao == 'J':
+            if option == 'J':
                 while True:
                     try:
-                        if pontos_jogador1 <=0 or pontos_jogador2 <=0:
-                            if pontos_jogador1 <=0 and pontos_jogador2 > 0:
-                                print(f'{jogador2} Venceu')
-                            if pontos_jogador2 <=0 and pontos_jogador1 > 0:
-                                print(f'{jogador1} Venceu')
-                            if pontos_jogador1 <= 0 and pontos_jogador2 <= 0:
+                        if player1_points <=0 or player2_points <=0:
+                            if player1_points <=0 and player2_points > 0:
+                                print(f'{player2} Venceu')
+                            if player2_points <=0 and player1_points > 0:
+                                print(f'{player1} Venceu')
+                            if player1_points <= 0 and player2_points <= 0:
                                 print('Empate')
                             exit()
                             break
-                        apostaj1 = int(input(f'Quantos pontos {jogador1}: '))
-                        apostaj2 = int(input(f'Quantos pontos {jogador2}: '))
-                        if apostaj1 <= 0 or apostaj1 > pontos_jogador1:
-                            print(f'{jogador1}O valor deve ser maior que 0, seus pontos são {pontos_jogador1}: ')
-                        if apostaj2 <= 0 or apostaj2 > pontos_jogador2:
-                            print(f'{jogador2}O valor deve ser maior que 0, seus pontos são {pontos_jogador1}: ')
+                        apostaj1 = int(input(f'Quantos pontos {player1}: '))
+                        apostaj2 = int(input(f'Quantos pontos {player2}: '))
+                        if apostaj1 <= 0 or apostaj1 > player1_points:
+                            print(f'{player1}O valor deve ser maior que 0, seus pontos são {player1_points}: ')
+                        if apostaj2 <= 0 or apostaj2 > player2_points:
+                            print(f'{player2}O valor deve ser maior que 0, seus pontos são {player1_points}: ')
                         else:
-                            low_highj1 = input(f'{jogador1}|[L] = 123 | [H] = 456 | [S] = Sair: ').upper()
-                            low_highj2 = input(f'{jogador2}|[L] = 123 | [H] = 456 | [S] = Sair: ').upper()
+                            low_highj1 = input(f'{player1}|[L] = 123 | [H] = 456 | [S] = Sair: ').upper()
+                            low_highj2 = input(f'{player2}|[L] = 123 | [H] = 456 | [S] = Sair: ').upper()
                             break
                     except ValueError:
                         print('valor inválido, digite apenas números inteiros.')
@@ -142,22 +142,22 @@ while jogadores == 2:
                     low_highj2 = input('|[L] = 123 | [H] = 456 | [S] = Sair: ').upper()
 
                 
-                if low_highj1 == valor and (low_highj1 == 'L' or low_highj1 == 'H'):
-                    print(f'{jogador1} Você acertou e ganhou {apostaj1} pontos!!')
-                    pontos_jogador1 = pontos_jogador1 + apostaj1
+                if low_highj1 == value and (low_highj1 == 'L' or low_highj1 == 'H'):
+                    print(f'{player1} Você acertou e ganhou {apostaj1} pontos!!')
+                    player1_points = player1_points + apostaj1
                     menu = False
                 elif low_highj1 == 'L' or low_highj1 == 'H':
-                    print(f'{jogador1} Você errou e perdeu {apostaj1} pontos!!')
-                    pontos_jogador1 = pontos_jogador1 - apostaj1
+                    print(f'{player1} Você errou e perdeu {apostaj1} pontos!!')
+                    player1_points = player1_points - apostaj1
                     menu = False
 
-                if low_highj2 == valor and (low_highj2 == 'L' or low_highj2 == 'H'):
-                    print(f'{jogador2} Você acertou e ganhou {apostaj2} pontos!!')
-                    pontos_jogador2 = pontos_jogador2 + apostaj2
+                if low_highj2 == value and (low_highj2 == 'L' or low_highj2 == 'H'):
+                    print(f'{player2} Você acertou e ganhou {apostaj2} pontos!!')
+                    player2_points = player2_points + apostaj2
                     menu = False
                 elif low_highj2 == 'L' or low_highj2 == 'H':
-                    print(f'{jogador2} Você errou e perdeu {apostaj2} pontos!!')
-                    pontos_jogador2 = pontos_jogador2 - apostaj2
+                    print(f'{player2} Você errou e perdeu {apostaj2} pontos!!')
+                    player2_points = player2_points - apostaj2
                     menu = False
                     
                 if low_highj1 == 'S' or low_highj2 == 'S':
@@ -165,22 +165,22 @@ while jogadores == 2:
                     break
                  
                 print()
-                print(f'Dado: {na} = {valor}')
+                print(f'Dado: {dice} = {value}')
 
-                print(f'{jogador1} Seus pontos: {pontos_jogador1}')
-                print(f'{jogador2} Seus pontos: {pontos_jogador2}')
+                print(f'{player1} Seus pontos: {player1_points}')
+                print(f'{player2} Seus pontos: {player2_points}')
                 print('=' * 30)
 
-            if opcao == 'S':
+            if option == 'S':
                 exit()
-                pontos_jogador1 = 1000
-                pontos_jogador2 = 1000
-            if opcao == 'P1':
-                print(f'{jogador1} possui {pontos_jogador1} pontos.')
-            if opcao == 'P2':
-                print(f'{jogador2} possui {pontos_jogador2} pontos.')
+                player1_points = 1000
+                player2_points = 1000
+            if option == 'P1':
+                print(f'{player1} possui {player1_points} pontos.')
+            if option == 'P2':
+                print(f'{player2} possui {player2_points} pontos.')
 
             if menu == True:
                 print('=' * 30)
-                opcao = input('Opção: ').upper()
+                option = input('Opção: ').upper()
                 print('=' * 30)
